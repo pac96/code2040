184705a51783783503cd0ac5989b519c54ce859d
Peter Clarke <pclarke@Peters-MacBook-Pro.local>
2016-10-10 19:20:58 -0400

    Wrote a function to connect to the registration endpoint

---
+++ connect.py
@@ -0,0 +1,32 @@
+import urllib
+import urllib2
+
+api_token = "dc9daf749ac1b14787b989fe1b97d42c"
+repository = "https://github.com/pac96/code2040-challenge"
+
+"""
+Connects to the CODE2040 challenge endpoint and sends the required
+data in JSON format. 
+
+"""
+def connect():
+    # Connect to the challenge endpoint
+    challenge_endpoint = "http://challenge.code2040.org/api/register"
+
+    # Send JSON with token and github repository
+    json = {"token" : api_token, 
+            "github" : repository}
+    data = urllib.urlencode(json)
+    req = urllib2.Request(challenge_endpoint, data)
+    response = urllib2.urlopen(req)
+    print "RESPONSE: " + response
+
+
+
+def main():
+    connect()
+
+
+
+if __name__ == "__main__":
+    main()
