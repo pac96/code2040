101f3c39bd291d0f85c4df9b4afc2f180a642442
Peter Clarke <pclarke@Peters-MacBook-Pro.local>
2016-10-15 15:27:42 -0400

    Fixed up some comments

--- dates.py
+++ dates.py
@@ -1,15 +1,11 @@
 import requests
 import arrow
 
-# The code below is for step 4 of the CODE2040 API Challenge
-# I've decided that since these are just supposed to be quick Python
-# scripts, I won't do all of the boilerplate and make a main method
-# and helper functions
+# Step 5 of CODE2040 API Challenge
 
 api_token = "dc9daf749ac1b14787b989fe1b97d42c"
 challenge_endpoint = "http://challenge.code2040.org/api/dating"
 validation_endpoint = "http://challenge.code2040.org/api/dating/validate"
-
 
 # Post token to endpoint and retrieve the datestamp and interval
 token_json = {"token" : api_token}
@@ -24,6 +20,8 @@ arrow_date = arrow.get(datestamp)
 new_date_timestamp = interval + arrow_date.timestamp
 
 # Convert the new date (now in seconds) back to UTC
+# I had to make sure that I added in the Z for zulu time to correspond to
+# the format the API gave us the date in
 final_utc_date = arrow.Arrow.utcfromtimestamp(new_date_timestamp).format("YYYY-MM-DDTHH:mm:ss") + "Z"
 
 # Validate your array by sending it back to the validation endpoint
--- prefix.py
+++ prefix.py
@@ -1,6 +1,6 @@
 import requests
 
-# The code below is for step 4 of the CODE2040 API Challenge
+# Step 4 of the CODE2040 API Challenge
 # I've decided that since these are just supposed to be quick Python
 # scripts, I won't do all of the boilerplate and make a main method
 # and helper functions
@@ -10,24 +10,21 @@ challenge_endpoint = "http://challenge.code2040.org/api/prefix"
 validation_endpoint = "http://challenge.code2040.org/api/prefix/validate"
 ret_list = []
 
-
 # Post token to endpoint
 json = {"token" : api_token}
 res = requests.post(challenge_endpoint, data=json)
 json_dict = res.json();
-
 
 # Get the prefix and the array from the json dictionary
 prefix = json_dict.get("prefix")
 prefix_len = len(prefix)
 arr = json_dict.get("array")
 
-
 # Get the elements that do not have the specified prefix in the array
 for elt in arr:
+	# Compare the part of the element 
 	if elt[:prefix_len] != prefix:
 		ret_list.append(elt)
-
 
 # Validate your array by sending it back to the validation endpoint
 ret_json = {"token" : api_token, "array" : ret_list}
